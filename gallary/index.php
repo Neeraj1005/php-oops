563f9da71f1aa721acd02c4edc95b3ebaab4796a
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-04-24 17:49:35 +0530

    section19 front end completed

--- gallary/index.php
+++ gallary/index.php
@@ -1,30 +1,45 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+
+$photos = Photo::find_all();
+
+
+
+?>
+<div class="row">
+
+    <!-- Blog Entries Column -->
+    <div class="col-md-12">
 
         <div class="row">
+            <?php foreach ($photos as $photo) { ?>
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+                <div class="col-xs-6 col-md-3">
 
-    
-            
-          
-         
+                    <a class="thumbnail" href="photo.php?id=<?php echo $photo->id ;?>">
+                        <img class="img-responsive custome_thumb" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                    </a>
 
-            </div>
+                </div>
 
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
+            <?php } ?>
         </div>
-        <!-- /.row -->
 
-        <?php include("includes/footer.php"); ?>
+    </div>
+
+
+
+
+    <!-- Blog Sidebar Widgets Column -->
+    <!-- <div class="col-md-4"> -->
+
+
+        <?php //include("includes/sidebar.php"); 
+        ?>
+
+    <!-- </div> -->
+</div>
+<!-- /.row -->
+
+<?php include("includes/footer.php"); ?>
