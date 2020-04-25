ab2aeaa4a728dc0279e7426f556e879ade152cfe
Neeraj Singh <43599636+Neeraj1005@users.noreply.github.com>
2020-04-25 21:46:59 +0530

    datatable added

---
+++ gallary/gallary_db.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2020 at 06:13 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gallary_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `photo_id`, `author`, `body`) VALUES
+(9, 11, 'Voluptate voluptas u', 'Nulla beatae rerum s'),
+(10, 11, 'Nihil sunt fuga Lab', 'Ut quasi sit asperi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `caption` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `alternate_text` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `photos`
+--
+
+INSERT INTO `photos` (`id`, `title`, `caption`, `description`, `alternate_text`, `filename`, `type`, `size`, `user_id`) VALUES
+(11, 'Voluptas dolor rerum', '', '', '', 'images-39.jpg', 'image/jpeg', 24969, 0),
+(12, 'Non veritatis offici', '', '', '', 'images-44.jpg', 'image/jpeg', 29486, 1),
+(13, 'Non harum velit sint', '', '', '', 'images-31.jpg', 'image/jpeg', 20928, 0),
+(14, 'Laboriosam nihil es', '', '', '', 'images-40.jpg', 'image/jpeg', 24385, 1),
+(15, 'Blog', '', '', '', '_large_image_1.jpg', 'image/jpeg', 479843, 0),
+(16, 'Quas asperiores null', '', '', '', '_large_image_3.jpg', 'image/jpeg', 165053, 0),
+(23, '', '', '', '', 'about.jpg', 'image/jpeg', 915107, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `user_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `user_image`) VALUES
+(1, 'admin', 'admin', 'John ', 'Doe', '_large_image_1.jpg'),
+(24, 'vivosude', 'Pa$$w0rd!', 'Janna', 'Clements', '_large_image_3.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index` (`photo_id`);
+
+--
+-- Indexes for table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
